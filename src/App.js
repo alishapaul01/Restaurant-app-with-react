0137112314b468a47a2e13c14950c19d6287b02a
alishapaul01 <paulalisha32@gmail.com>
2023-08-02 18:17:16 +0530

    Added Summary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import Header from './Components/Layout/Header';
+import Summary from './Components/Meals/Summary.js';
 const App=()=>{
  return(
   <>
     <Header/>
+    <Summary/>
   </>
  )
 }
