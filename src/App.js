94f9969a4abd04e3b23a23c9a35bd034daa8a964
alishapaul01 <paulalisha32@gmail.com>
2023-08-07 15:47:15 +0530

    Cart open and close functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 
 import './App.css';
+import { useState } from 'react';
 import Header from './Components/Layout/Header';
 import DummyMeals from './Components/Meals/DummyMeals';
 import Summary from './Components/Meals/Summary.js';
 import Cart from './Components/Cart/Cart';
+
 const App=()=>{
+  const[cartIsShown, setCartIsShown]= useState(false);
+  const showCartHandler= ()=>{
+    setCartIsShown(true);
+  }
+  const hideCartHandler=()=>{
+    setCartIsShown(false);
+  }
  return(
   <>
-    <Cart/>
-    <Header/>
+    {cartIsShown && <Cart onClose={hideCartHandler}/>}
+    <Header onShowCart= {showCartHandler}/>
     <Summary/>
     <DummyMeals/>
   </>
